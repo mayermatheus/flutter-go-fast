972d38c3e8765869379c7990ef80cc58f1e8bf84
Matheus Mayer <matheusmayerduarte@gmail.com>
2020-08-01 16:06:17 -0300

    Convertendo a homePage para stateful a fim de obter a ligação com o controller

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_go_fast/app/modules/home/home_controller.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends ModularState<HomePage, HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold();
