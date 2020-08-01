aed560b34dc3d77f32834c74a0227cc50355836a
Matheus Mayer <matheusmayerduarte@gmail.com>
2020-08-01 16:05:55 -0300

    Adicionando splash na tela inicial do modulo da aplicação

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -2,6 +2,7 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_go_fast/app/app_controller.dart';
 import 'package:flutter_go_fast/app/app_widget.dart';
 import 'package:flutter_go_fast/app/modules/home/home_module.dart';
+import 'package:flutter_go_fast/app/modules/splash/splash_page.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class AppModule extends MainModule {
@@ -17,8 +18,11 @@ class AppModule extends MainModule {
 
   @override
   List<Router> get routers => [
+        Router('/',
+            child: (_, args) => SplashPage(),
+            transition: TransitionType.noTransition),
         Router(
-          '/',
+          '/home',
           module: HomeModule(),
         ),
       ];
